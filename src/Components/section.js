ca5159bc37762d7aa54c04d5d5309edd1f9f424d
SureshK2004 <sureshksureshk04gmail.com>
2025-06-13 22:59:08 +0530

    api la iruthu eduthachi

--- src/Components/section.js
+++ src/Components/section.js
@@ -1,30 +1,25 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 
-function Homesection(){
-    const[product,setProduct] =useState([])
+function Homesection() {
+  const [category, setCategory] = useState([]);
 
-    useEffect(()=>{
-        fetch("https://fakestoreapi.com/products")
-            .then(data => data.json())
-            .then(json => setProduct(json))
-             console.log(product)
-            
-    },[]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((res) => res.json())
+      .then((json) => setCategory(json))
+  }, []);
 
-
-    return(
-       <>
-       {product.map((data)=>(
-        <>
-        <ul>
-            <li>{data.id}</li>
-            <li>{data.title}</li>
-        </ul>
-        </>
-       ))}
-       </>
-    )
-
+  return (
+    <div className="row m-5">
+      {category.map((data) => (
+        <div className="col-md-3" >
+          <div className="card h-100 text-center p-3">
+            <h5 className="card-title text-capitalize">{data}</h5>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Homesection;
