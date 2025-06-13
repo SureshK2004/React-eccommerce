d8d2314b039014d126018a3f5e906f216e580083
SureshK2004 <sureshksureshk04gmail.com>
2025-06-13 21:11:13 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Components/header";
+import Homesection from "./Components/section";
 import Footer from "./Components/footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Homesection/>
       <Footer />
     </div>
   );
--- src/Components/section.js
+++ src/Components/section.js
@@ -3,7 +3,28 @@ import React,{useEffect,useState} from "react";
 function Homesection(){
     const[product,setProduct] =useState([])
 
-    useEffect
-    return();
+    useEffect(()=>{
+        fetch("https://fakestoreapi.com/products")
+            .then(data => data.json())
+            .then(json => setProduct(json))
+             console.log(product)
+            
+    },[]);
+
+
+    return(
+       <>
+       {product.map((data)=>(
+        <>
+        <ul>
+            <li>{data.id}</li>
+            <li>{data.title}</li>
+        </ul>
+        </>
+       ))}
+       </>
+    )
 
 }
+
+export default Homesection;
